f1cf35c1b50346399cad8e5392398a83e97d8fe7
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-22 20:36:11 +0600

    image from api

--- api.js
+++ api.js
@@ -9,3 +9,17 @@ function getUser() {
         .then(data => console.log(data))
 }
 
+const imageBlock = document.getElementById('img-block');
+fetch('https://jsonplaceholder.typicode.com/photos/1')
+    .then(response => response.json())
+    .then(data => showImage(data))
+
+function showImage(data) {
+    const image = document.createElement('img');
+    image.src = data.url;
+    image.id = data.id;
+    image.title = data.title;
+    image.width = 150;
+    imageBlock.appendChild(image);
+}
+
